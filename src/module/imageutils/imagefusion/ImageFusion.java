f64192aa1b80807ec7e39513be7a53d1e580b35f
kshea12 <kshea12@gmail.com>
2016-03-24 12:30:32 -0400

    Added Haar Transformation

--- src/module/imageutils/imagefusion/ImageFusion.java
+++ src/module/imageutils/imagefusion/ImageFusion.java
@@ -4,4 +4,31 @@ package module.imageutils.imagefusion;
  * Created by kshea12 on 3/22/16.
  */
 public class ImageFusion {
+
+    public static double[][] averageFusion(double[][] first, double[][] second){
+        int w = first[0].length;
+        int h = first.length;
+        double[][] fused = new double[h][w];
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++) {
+                fused[i][j] = (first[i][j] + second[i][j]) / 2;
+            }
+        }
+        return fused;
+    }
+
+    public static double[][] maxFusion(double[][] first, double[][] second){
+        int w = first[0].length;
+        int h = first.length;
+        double[][] fused = new double[h][w];
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++) {
+                if(first[i][j] > second[i][j])
+                    fused[i][j] = first[i][j];
+                else
+                    fused[i][j] = second[i][j];
+            }
+        }
+        return fused;
+    }
 }
